0ac1cd93a8adc43f13e7f72636e4fce3bdfe9350
三拳超人 <949608491@qq.com>
2019-03-08 09:58:38 +0800

    删除了Java基础练习

--- Java基础练习/yunsi/src/com/yunsi/shop/Menu.java
+++
@@ -1,7 +0,0 @@
-package com.yunsi.shop;
-
-public class Menu {
-	public static void main(String[] args) {
-		System.out.println("Œ“∫√Àß");
-	}
-}
--- Java基础练习/yunsi/src/com/yunsi/shop/Ojj.java
+++
@@ -1,7 +0,0 @@
-package com.yunsi.shop;
-
-public class Ojj {
-	public static void main(String[] args) {
-		System.out.println("aa");
-	}
-}
